5d032885e9d586e10073911455588e0f4de269be
rohitzGit <38519671+rohitzGit@users.noreply.github.com>
2018-12-09 19:04:18 +0530

    Completed the module

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
-//import Quiz from './Quiz.js'
+import Quiz from './Quiz'
 import './App.css'
 
-let quizData = require('./quiz_data.json')
+// let quizData = require('./quiz_data.json')
 
-class Quiz extends Component{
-    constructor(props){
-        super(props)
-        this.state = {quiz_position: 1}
-    }
-    render(){
-        return(
-            <div>
-                <div className="QuizQuestion">
-                    {quizData.quiz_questions[0].instruction_text}
-                </div>
-            </div>
-        )
-    }
-}
+// class Quiz extends Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {quiz_position: 1}
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <div className="QuizQuestion">
+//                     {quizData.quiz_questions[0].instruction_text}
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 class App extends Component {
   render() {
